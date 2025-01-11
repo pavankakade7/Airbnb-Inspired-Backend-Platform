8e306675182c8ea2e02f07f48becaec9341f1a41
Anuj Kumar Sharma <anuj55149@gmail.com>
2025-01-12 04:01:42 +0530

    Part - 6, Payments Support added

--- src/main/java/com/codingshuttle/projects/airBnbApp/service/BookingService.java
+++ src/main/java/com/codingshuttle/projects/airBnbApp/service/BookingService.java
@@ -3,12 +3,22 @@ package com.codingshuttle.projects.airBnbApp.service;
 import com.codingshuttle.projects.airBnbApp.dto.BookingDto;
 import com.codingshuttle.projects.airBnbApp.dto.BookingRequest;
 import com.codingshuttle.projects.airBnbApp.dto.GuestDto;
+import com.stripe.model.Event;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BookingService {
 
     BookingDto initialiseBooking(BookingRequest bookingRequest);
 
     BookingDto addGuests(Long bookingId, List<GuestDto> guestDtoList);
+
+    String initiatePayments(Long bookingId);
+
+    void capturePayment(Event event);
+
+    void cancelBooking(Long bookingId);
+
+    String getBookingStatus(Long bookingId);
 }
